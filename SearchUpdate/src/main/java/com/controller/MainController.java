3998b3922098b0fbb464fe59d9235c6865e529d4
Dhaneshan <102854002+Dhaneshan@users.noreply.github.com>
2022-08-26 22:10:19 +0530

    Add files via upload

---
+++ SearchUpdate/src/main/java/com/controller/MainController.java
@@ -0,0 +1,68 @@
+package com.controller;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.dao.EProductDao;
+import com.entity.EProductEntity;
+import org.apache.log4j.Logger;
+@Controller
+public class MainController {
+	@Autowired
+	EProductDao dao;
+	private Logger logger = Logger.getLogger(MainController.class);
+	@GetMapping("/productid")
+	public String getProduct(HttpServletRequest request,ModelMap map, HttpServletResponse response) {
+		long id=Long.parseLong(request.getParameter("id"));	
+		if(id>0)
+		{
+			EProductEntity entity=dao.getProductById(id);
+			
+			logger.info("This Is An Info Log Entry ......!");
+	        logger.info(entity);
+			map.addAttribute("obj",entity);
+			return "details";
+		}
+		else
+		{
+			
+				try 
+				{
+					request.getRequestDispatcher("/index.jsp").include(request, response);
+					response.setContentType("text/html");
+					PrintWriter out = response.getWriter();
+					out.println("Oops!!Invalid Credentials:)");				
+					
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		return null;
+	}
+	
+	
+	@GetMapping("/update")
+	public String updateProduct(HttpServletRequest request,ModelMap map) {
+		long id=Long.parseLong(request.getParameter("id"));
+		String name=request.getParameter("name");
+		BigDecimal price = new BigDecimal(request.getParameter("price"));
+ {
+			EProductEntity entity= new EProductEntity();
+			entity.setId(id);
+			entity.setName(name);
+			entity.setPrice(price);
+			dao.updateProduct(entity);
+			map.addAttribute("obj",entity);
+			return "details";
+			
+		} 
+
+	}
+
+}
---
+++ SearchUpdate/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Please enter the product id for search</h2>
+<form action="Productid">
+<input type="number" name="id" placeholder="Enter id">
+<input type="submit" value="Enter">
+</form>
+</body>
+</html>
